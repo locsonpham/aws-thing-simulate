b37acd1653a7391e4902ef6e9877947d494ec291
Loc Pham <loc.pham@mitechvn.com>
2022-01-21 18:13:54 +0700

    add handleDelta, device GET method

--- index.js
+++ index.js
@@ -2,40 +2,108 @@
 // SPDX-License-Identifier: MIT-0
 var awsIot = require("aws-iot-device-sdk");
 require("dotenv").config();
+const { DeviceModel } = require("./model/device");
 
-//
-// Replace the values of '<YourUniqueClientIdentifier>' and '<YourCustomEndpoint>'
-// with a unique client identifier and custom host endpoint provided in AWS IoT cloud
-// NOTE: client identifiers must be unique within your AWS account; if a client attempts
-// to connect with a client identifier which is already in use, the existing
-// connection will be terminated.
-//
 var clientId = process.env.CLIENT_ID;
 
-// var device = awsIot.device({
-//   keyPath: process.env.AWS_PRIVATE_KEY_PATH,
-//   certPath: process.env.AWS_CERT_PATH,
-//   caPath: process.env.AWS_CA_PATH,
-//   clientId: process.env.CLIENT_ID,
-//   host: process.env.AWS_HOST,
-// });
+const initDeviceData = {
+  vID: clientId,
+  deviceSerial: "dmt_esocket_123456",
+  deviceProfile: "dmt_esocket_1sw",
+  attributes: {
+    hardware: "1.3.4",
+    firmware: "1.6.2",
+    manufacturer: "OEM",
+  },
+  connection: {
+    wifi: {
+      status: "connected",
+      ssid: "FOOMAP.ASIA",
+      bssid: "67:AD:89:90",
+      ip: "192.168.0.9",
+      broadcast: "192.168.0.255",
+      gw: "192.168.0.1",
+    },
+    cellularNetwork: {
+      status: "disconnected",
+    },
+    iot_broker: {
+      status: "connected",
+    },
+    iot_server: {
+      status: "disconnected",
+    },
+  },
+  settings: {
+    stateReportTimeout: 5000,
+  },
+  automation: [
+    {
+      _id: 0, // id of automation script
+      type: "period",
+      enable: true,
+      startTime: "17:00:00",
+      endTime: "19:00:00",
+      repeat: "daily",
+      action: [
+        {
+          switch0: "on",
+        },
+      ],
+    },
+  ],
+  fields: [
+    {
+      fkey: "temperature",
+      readonly: true,
+      reportedVal: null,
+      desiredVal: null,
+    },
+    {
+      fkey: "humidity",
+      readonly: true,
+      reportedVal: null,
+      desiredVal: null,
+    },
+    {
+      fkey: "switch1",
+      readonly: false,
+      reportedVal: "off",
+      desiredVal: null,
+    },
+    {
+      fkey: "switch2",
+      readonly: false,
+      reportedVal: "off",
+      desiredVal: null,
+    },
+    {
+      fkey: "switch3",
+      readonly: false,
+      reportedVal: "off",
+      desiredVal: null,
+    },
+    {
+      fkey: "switch4",
+      readonly: false,
+      reportedVal: "off",
+      desiredVal: null,
+    },
+  ],
+};
 
-// //
-// // Device is an instance returned by mqtt.Client(), see mqtt.js for full
-// // documentation.
-// //
-// device.on("connect", function () {
-//   console.log("connect");
-//   device.subscribe(`demeter/things/${clientId}/#`);
-//   device.publish(
-//     `demeter/things/${clientId}/update`,
-//     JSON.stringify({ test_data: 1 })
-//   );
-// });
+var device = new DeviceModel(initDeviceData);
+device.init();
+setInterval(() => {
+  device.simulateData();
+}, 5000);
 
-// device.on("message", function (topic, payload) {
-//   console.log("message", topic, payload.toString());
-// });
+const deviceTopics = [
+  `demeter/things/${clientId}/get`,
+  `demeter/things/${clientId}/set`,
+  `demeter/things/${clientId}/telemetry/update/accepted`,
+  `demeter/things/${clientId}/telemetry/update/rejected`,
+];
 
 var thingShadows = awsIot.thingShadow({
   keyPath: process.env.AWS_PRIVATE_KEY_PATH,
@@ -45,90 +113,77 @@ var thingShadows = awsIot.thingShadow({
   host: process.env.AWS_HOST,
 });
 
-//
-// Client token value returned from thingShadows.update() operation
-//
 var clientTokenUpdate;
 
-//
-// Simulated device values
-//
-var rval = 187;
-var gval = 114;
-var bval = 222;
-
 thingShadows.on("connect", function () {
-  //
-  // After connecting to the AWS IoT platform, register interest in the
-  // Thing Shadow named 'RGBLedLamp'.
-  //
-  // console.log("shadow connected to AWS");
-  // thingShadows.subscribe(`demeter/things/${clientId}/#`);
-  // thingShadows.publish(
-  //   `demeter/things/${clientId}/update`,
-  //   JSON.stringify({ test_data: 1 })
-  // );
+  console.log(`client-${clientId} connected to AWS`);
+
+  thingShadows.subscribe(deviceTopics, (err, granted) => {
+    if (err) {
+      console.log(err);
+    } else {
+      // console.log(granted);
+    }
+  });
 
   thingShadows.register(clientId, {}, function () {
-    // Once registration is complete, update the Thing Shadow named
-    // 'RGBLedLamp' with the latest device state and save the clientToken
-    // so that we can correlate it with status or timeout events.
-    //
-    // Thing shadow state
-    //
+    console.log(`shadow-${clientId} is registered`);
 
-    console.log("shadow registered: " + clientId);
+    clientTokenUpdate = device.reportDeviceState(thingShadows);
 
-    var rgbLedLampState = {
-      state: { desired: { red: rval, green: gval, blue: bval } },
-    };
-
-    clientTokenUpdate = thingShadows.update(clientId, rgbLedLampState);
-    //
-    // The update method returns a clientToken; if non-null, this value will
-    // be sent in a 'status' event when the operation completes, allowing you
-    // to know whether or not the update was successful.  If the update method
-    // returns null, it's because another operation is currently in progress and
-    // you'll need to wait until it completes (or times out) before updating the
-    // shadow.
-    //
-    if (clientTokenUpdate === null) {
-      console.log("update shadow failed, operation still in progress");
+    if (true) {
+      setInterval(() => {
+        device.reportDeviceState(thingShadows);
+      }, device.settings.stateReportTimeout);
     }
   });
 });
 
 thingShadows.on("message", function (topic, payload) {
-  console.log("message", topic, payload.toString());
+  // console.log("message", topic, payload.toString());
+  const topics = topic.split("/");
+  console.log(topics);
+
+  if (
+    topics.length >= 4 &&
+    topics[0] == "demeter" &&
+    topics[1] == "things" &&
+    topics[2] == clientId
+  ) {
+    switch (topics[3]) {
+      case "get":
+        {
+          device.handleGet(thingShadows, payload);
+        }
+        break;
+      case "set":
+        {
+          device.handleSet(thingShadows, payload);
+        }
+        break;
+      default:
+        break;
+    }
+  }
 });
 
 thingShadows.on("status", function (thingName, stat, clientToken, stateObject) {
   console.log(
     "received " + stat + " on " + thingName + ": " + JSON.stringify(stateObject)
   );
-  //
-  // These events report the status of update(), get(), and delete()
-  // calls.  The clientToken value associated with the event will have
-  // the same value which was returned in an earlier call to get(),
-  // update(), or delete().  Use status events to keep track of the
-  // status of shadow operations.
-  //
+  console.log(clientToken);
 });
 
 thingShadows.on("delta", function (thingName, stateObject) {
-  console.log(
-    "received delta on " + thingName + ": " + JSON.stringify(stateObject)
-  );
+  // console.log(
+  //   "received delta on " + thingName + ": " + JSON.stringify(stateObject)
+  // );
+  device.deltaUpdate(thingShadows, stateObject);
+  // reportDeviceState();
 });
 
 thingShadows.on("timeout", function (thingName, clientToken) {
   console.log(
     "received timeout on " + thingName + " with token: " + clientToken
   );
-  //
-  // In the event that a shadow operation times out, you'll receive
-  // one of these events.  The clientToken value associated with the
-  // event will have the same value which was returned in an earlier
-  // call to get(), update(), or delete().
-  //
 });
